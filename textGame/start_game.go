dd195f8b0ce48edce9ac95c3e8b8cd886f59ac41
sabnitor <viacheslavkrutiy@gmail.com>
2023-09-19 19:34:23 +0300

    done

--- textGame/start_game.go
+++ textGame/start_game.go
@@ -3,16 +3,16 @@ package textgame
 import "fmt"
 
 //Castle actions
-var talkToKing = action{"You try to talk with King", []string{"Ask for lands", "Tell about myself"}}
-var talkGuardians = action{"You try to talk with Guardiavns", []string{"Information about King", "Ask for job"}}
+var talkToKing = action{"Try to talk with King", []string{"Ask for lands", "Tell about myself"}}
+var talkGuardians = action{"Try to talk with Guardiavns", []string{"Information about King", "Ask for job"}}
 
 //Central Square actions
-var cadge = action{"You try to cadge some money", []string{"Shout to the whole square", "Sit with outstretched arm"}}
-var arrangeDuel = action{"You try arrange a duel", []string{"Arrange a duel with knight", "Arrange a duel with chicken"}}
+var cadge = action{"Try to cadge some money", []string{"Shout to the whole square", "Sit with outstretched arm"}}
+var arrangeDuel = action{"Try arrange a duel", []string{"Arrange a duel with knight", "Arrange a duel with chicken"}}
 
 //Forest actions
-var chopOaks = action{"You try to chop oaks", []string{"Chop huge oak", "Chop small oak"}}
-var hunt = action{"You try to hunt", []string{"Hunt a hare", "Hunt a deer"}}
+var chopOaks = action{"Try to chop oaks", []string{"Chop huge oak", "Chop small oak"}}
+var hunt = action{"Try to hunt", []string{"Hunt a hare", "Hunt a deer"}}
 
 var castle = location{"Castle", []action{talkToKing, talkGuardians}}
 var centralSquare = location{"Central Square", []action{cadge, arrangeDuel}}
@@ -20,22 +20,68 @@ var forest = location{"Forest", []action{chopOaks, hunt}}
 
 //character actions
 func whatToDo(c *character) {
-	var input string
+	var input1 string
+	var input2 string
 	curentEvents := make([]string, 0)
 	for _, v := range c.curentLocation.actions {
 		curentEvents = append(curentEvents, v.discroption)
 	}
+loop:
 	fmt.Printf("Your location is %s\nWhat do you want?\n", c.curentLocation.name)
 	for i, item := range curentEvents {
 		fmt.Printf("%d) %s\n", i+1, item)
 	}
-	fmt.Scan(&input)
-	switch input {
+	fmt.Scan(&input1)
+	switch input1 {
 	case "1":
+		curentEvents = c.curentLocation.actions[0].choise
+		fmt.Println("What do you want?")
+		for i, item := range curentEvents {
+			fmt.Printf("%d) %s\n", i+1, item)
+		}
+		fmt.Scan(&input2)
 
 	case "2":
+		curentEvents = c.curentLocation.actions[1].choise
+		fmt.Println("What do you want?")
+		for i, item := range curentEvents {
+			fmt.Printf("%d) %s\n", i+1, item)
+		}
+		fmt.Scan(&input2)
+
+	default:
+		fmt.Println("Try again!")
+		goto loop
 	}
 
+	if c.curentLocation.name == "Castle" && input1 == "1" && input2 == "1" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Castle" && input1 == "1" && input2 == "2" {
+		c.becameTheKing()
+	} else if c.curentLocation.name == "Castle" && input1 == "2" && input2 == "1" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Castle" && input1 == "2" && input2 == "2" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Central Square" && input1 == "1" && input2 == "1" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Central Square" && input1 == "1" && input2 == "2" {
+		c.eat()
+	} else if c.curentLocation.name == "Central Square" && input1 == "2" && input2 == "1" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Central Square" && input1 == "2" && input2 == "2" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Forest" && input1 == "1" && input2 == "1" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Forest" && input1 == "1" && input2 == "2" {
+		c.takeDamage()
+	} else if c.curentLocation.name == "Forest" && input1 == "2" && input2 == "1" {
+		c.eat()
+	} else if c.curentLocation.name == "Forest" && input1 == "2" && input2 == "2" {
+		c.takeDamage()
+	} else {
+		fmt.Println("Try again!")
+		goto loop
+	}
 }
 
 func whereToGo(c *character) {
