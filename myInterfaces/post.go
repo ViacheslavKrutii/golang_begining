257214feede2779c4ad0b67489f838fc57404000
sabnitor <viacheslavkrutiy@gmail.com>
2023-09-28 11:42:10 +0300

    post

--- myInterfaces/post.go
+++ myInterfaces/post.go
@@ -1,16 +1,22 @@
 package myinterfaces
 
+type content struct {
+}
+
 type Parcel interface {
-	send()
+	send(from, to string) Parcel
 }
 
 type envelope struct {
 	addressOfSender    string
 	addressOfRecipient string
+	content            content
 }
 
-func (e envelope) send() {
-
+func (e envelope) send(from, to string) Parcel {
+	e.addressOfSender = from
+	e.addressOfRecipient = to
+	return e
 }
 
 type box struct {
@@ -18,6 +24,21 @@ type box struct {
 	addressOfRecipient string
 }
 
-func (b box) send() {
+func (b box) send(from, to string) Parcel {
+	b.addressOfSender = from
+	b.addressOfRecipient = to
+	return b
+}
 
+func sortSection(p Parcel, b, e chan Parcel) {
+	p, ok := p.(box)
+	if ok {
+		b <- p
+		return
+	}
+	p, ok = p.(envelope)
+	if ok {
+		e <- p
+		return
+	}
 }
